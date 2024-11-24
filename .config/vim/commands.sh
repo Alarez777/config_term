f3517ee0a5c966812fb282e1a5d52bc51c1ce931
alarez <alarez@macbookpro13debian.lan>
2024-11-23 23:13:59 -0600

    add commands

---
+++ .config/vim/commands.sh
@@ -0,0 +1,4 @@
+ln -s ~/.config/config_term/.config/vim ~/.config/vim
+ln -s ~/.config/vim/init.vim ~/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
